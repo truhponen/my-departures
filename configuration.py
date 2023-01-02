6b704e973b89c01496a2bb8bc35e6fc83c827e2e
Truhponen <truhponen@gmail.com>
2023-01-03 00:10:51 +0200

    Config file change

--- configuration.py
+++ configuration.py
@@ -7,8 +7,8 @@ import yaml
 class yaml_configurations():
 
     def __init__(self):
-        file_app = open("data/config.yaml", "r")
-        file_rest = open("data/rest_config.yaml", "r")
+        file_app = open("./data/config.yaml", "r")
+        file_rest = open("./data/rest_config.yaml", "r")
         self.app = yaml.load(file_app, Loader=yaml.FullLoader)
         self.rest = yaml.load(file_rest, Loader=yaml.FullLoader)
         self.date = date.today()
