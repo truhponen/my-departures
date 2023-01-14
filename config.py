805be38ac9ad32a75a044d683c8aea0ee9379487
Truhponen <truhponen@gmail.com>
2023-01-14 18:05:33 +0200

    removing operations config

--- config.py
+++ config.py
@@ -8,10 +8,8 @@ import yaml
 app_dir = "."
 file_app = open(app_dir + "/data/config.yaml", "r")
 file_rest = open(app_dir + "/data/rest_config.yaml", "r")
-file_operations = open(app_dir + "/data/operations_config.yaml", "r")
 app = yaml.load(file_app, Loader=yaml.FullLoader)
 rest = yaml.load(file_rest, Loader=yaml.FullLoader)
-operations = yaml.load(file_operations, Loader=yaml.FullLoader)
 date = date.today()
 db = app_dir + "/data/departures.json"
 
